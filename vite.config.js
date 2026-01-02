6c050df6063e56d56b4903f8a5c312d53e344e16
Damir <dambul2809@mail.ru>
2026-01-02 10:51:10 +0700

    Add 'build'

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import {resolve} from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +11,13 @@ export default defineConfig({
       },
     }),
   ],
-  base: '/dbdevtools-www/'
-})
+  base: '/dbdevtools-www/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        remixtree: resolve(__dirname, 'remixtree.html')
+      }
+    }
+  }
+});
